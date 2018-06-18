51c16754279b83b47df39443a4a09c6606277060
Beinor Fabian Calderon <fabian.cb@hotmail.es>
2018-06-18 15:33:09 -0600

    Metodo de Unit Test agregado

---
+++ BingoWcf/Bingo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bingo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bingo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("118b139d-6864-48f2-ad7a-77a4ab71b39b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BingoWcf/Bingo.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bingo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CantidadCartones()
+        {
+            // prepara el escenario
+            var elServicio = new BingoWcf.Dominio.Acciones.CrearCarton();
+            int num = 10;
+            var elServicio2 = new BingoWcf.Dominio.Acciones.CantidadCartones();
+            int expectedResult;
+            int realResult;
+            List<int[,]> cartonesResult = new List<int[,]>();
+            List<int[,]> cartonesExpected = new List<int[,]>();
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            cartonesExpected.Add(elServicio.CrearMatrizCarton());
+            expectedResult = cartonesExpected.Count;
+            // invoca al método
+            cartonesResult = elServicio2.cantidadCartones(num);
+            realResult = cartonesResult.Count;
+
+
+            // verifica resultados
+            Assert.AreEqual(expectedResult, realResult);
+        }
+    }
+}
